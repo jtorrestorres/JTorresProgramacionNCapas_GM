ca8004fdff4a5f8cecf8c6eabbd7eba94c4e38e4
Jesús Torres <jtorres@digis01.com>
2022-04-01 12:58:58 -0600

    Add project files.

---
+++ BL/Materia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BL
+{
+    public class Materia
+    {
+       //int 
+       //byte 0-255
+        public static void Add(ML.Materia materia) //Database
+        {//SQL Client
+
+            using (SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=JTorresProgramacionNCapas08122021;Persist Security Info=True;User ID=sa;Password=pass@word1"))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO [Materia]([Nombre],[Creditos],[Costo])VALUES (@Nombre,@Creditos, @Costo)", conn);
+                cmd.Parameters.AddWithValue("@Nombre", materia.Nombre);
+                cmd.Parameters.AddWithValue("@Creditos", materia.Creditos);
+                cmd.Parameters.AddWithValue("@Costo", materia.Costo);
+
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ PL/Materia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Materia
+    {
+        public static void Add()
+        {
+            ML.Materia materia = new ML.Materia();
+
+            Console.WriteLine("Ingrese el nombre de la materia");
+            materia.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese los creditos para la materia");
+            materia.Creditos = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el costo de la materia");
+            materia.Costo = decimal.Parse(Console.ReadLine());
+
+
+            BL.Materia.Add(materia);
+
+        }
+
+    }
+}
