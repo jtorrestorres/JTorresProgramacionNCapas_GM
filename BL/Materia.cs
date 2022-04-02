91c5b48931616bdafe75471b871f36c23f0b625a
AngelMendoza1 <89880077+AngelMendoza1@users.noreply.github.com>
2022-04-01 19:05:12 -0600

    Add

--- BL/Materia.cs
+++ BL/Materia.cs
@@ -12,20 +12,20 @@ namespace BL
     {
        //int 
        //byte 0-255
-        public static void Add(ML.Materia materia) //Database
-        {//SQL Client
+        //public static void Add(ML.Materia materia) //Database
+        //{//SQL Client
 
-            using (SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=JTorresProgramacionNCapas08122021;Persist Security Info=True;User ID=sa;Password=pass@word1"))
-            {
-                SqlCommand cmd = new SqlCommand("INSERT INTO [Materia]([Nombre],[Creditos],[Costo])VALUES (@Nombre,@Creditos, @Costo)", conn);
-                cmd.Parameters.AddWithValue("@Nombre", materia.Nombre);
-                cmd.Parameters.AddWithValue("@Creditos", materia.Creditos);
-                cmd.Parameters.AddWithValue("@Costo", materia.Costo);
+        //    using (SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=LEscogidoMarzo;User ID=sa;Password=pass@word1"))
+        //    {
+        //        SqlCommand cmd = new SqlCommand("INSERT INTO [Materia]([Nombre],[Creditos],[Costo])VALUES (@Nombre,@Creditos, @Costo)", conn);
+        //        cmd.Parameters.AddWithValue("@Nombre", materia.Nombre);
+        //        cmd.Parameters.AddWithValue("@Creditos", materia.Creditos);
+        //        cmd.Parameters.AddWithValue("@Costo", materia.Costo);
 
 
-                conn.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
+        //        conn.Open();
+        //        cmd.ExecuteNonQuery();
+        //    }
+        //}
     }
 }
